3f9b32c564d3d553a55ae27cefbb87c76b18c08b
Sunil Kumar <rsunil.kumar1803@gmail.com>
2025-09-16 22:07:41 +0530

    updated

--- src/test/java/factory/DriverFactory.java
+++ src/test/java/factory/DriverFactory.java
@@ -12,6 +12,8 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+
 public class DriverFactory {
 	static WebDriver driver = null;
 	
@@ -20,8 +22,13 @@ public class DriverFactory {
 	public static WebDriver initilizeBrowser(String browserName) {
 		
 		if(browserName.equalsIgnoreCase("chrome")) {
+			WebDriverManager.chromedriver().setup();
 			driver = new ChromeDriver();
+<<<<<<< HEAD
 
+=======
+			
+>>>>>>> 51367e4 (Updated)
 		}else if(browserName.equalsIgnoreCase("edge")) {
 			driver = new EdgeDriver();	
 		}
@@ -34,3 +41,7 @@ public class DriverFactory {
 	}
 
 }
+
+
+
+ 
